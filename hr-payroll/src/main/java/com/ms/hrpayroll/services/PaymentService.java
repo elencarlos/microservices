29b9aa4b8558ac4e0a3c3d67c1e4eaae12bde68f
Elencarlos Soares Silva <elencarlos@uft.edu.br>
2021-08-15 15:22:33 -0300

    add resttemplate

--- hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/ms/hrpayroll/services/PaymentService.java
@@ -1,12 +1,29 @@
 package com.ms.hrpayroll.services;
 
 import com.ms.hrpayroll.entities.Payment;
+import com.ms.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long id, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> uriParameters = new HashMap<>();
+        uriParameters.put("id", String.valueOf(id));
+        Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}"), Worker.class, uriParameters);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
 }
